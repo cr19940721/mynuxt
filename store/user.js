03756856bbc279689b1aba0058e43e6ac1367f8f
cr <455318321@qq.com>
2019-08-02 16:26:51 +0800

    1.01

---
+++ store/user.js
@@ -0,0 +1,36 @@
+export const state = () => ({
+    userInfo:{
+        token:'',
+        user:{}
+    }
+})
+
+export const mutations={
+    setUserInfo(state,data){
+        state.userInfo = data
+    },
+    cleanUserInfo(state,info){
+        if(process.browser){
+            localStorage.removeItem('userInfo')
+        }
+        state.userInfo = {}
+    }
+}
+
+export const actions={
+    login({commit},data){
+        return this.$axios({
+            url:'/accounts/login',
+            method:'POST',
+            data:data
+        })
+        .then(res=>{
+            const data = res.data
+            commit('setUserInfo',data)
+            return data
+        })
+        .catch(err=>{
+            console.log(err)
+        })
+    }
+}
